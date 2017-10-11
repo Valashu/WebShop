e0fcf112e6cc78667b4a84d9f2458363e185f06e
Sebastian Andreas Werthemann <werts1@bfh.ch>
2017-10-11 11:26:04 +0200

    init

---
+++ nav/nav.php
@@ -0,0 +1,12 @@
+<ul class="topnav">
+  <li><a href="#owl">Home</a></li>
+  <li><a href="#ramen">Ramen</a></li>
+  <li><a href="#about">About</a></li>
+  <li class="dropdown"><a href="#dropdown" class="dropbtn">Dropdown</a>
+    <div class="dropcontent">
+      <a href="#drop 1">Drop 1</a>
+      <a href="#drop 2">Drop 2</a>
+      <a href="#drop 7.9">Drop 3</a>
+    </div>
+  </li>
+</ul>
