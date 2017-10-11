781e58e69805080102721db76e35d1cd4da005ce
Sebastian Andreas Werthemann <werts1@bfh.ch>
2017-10-11 13:03:27 +0200

    added pages

--- nav/nav.php
+++ nav/nav.php
@@ -1,7 +1,7 @@
 <ul class="topnav">
-  <li><a href="#owl">Home</a></li>
-  <li><a href="#ramen">Ramen</a></li>
-  <li><a href="#about">About</a></li>
+  <li><a href="http://localhost/WebPrograms/WebShop/RamenShop.php">Home</a></li>
+  <li><a href="http://localhost/WebPrograms/WebShop/pages/ramen.php">Ramen</a></li>
+  <li><a href="http://localhost/WebPrograms/WebShop/pages/about.php">About</a></li>
   <li class="dropdown"><a href="#dropdown" class="dropbtn">Dropdown</a>
     <div class="dropcontent">
       <a href="#drop 1">Drop 1</a>
