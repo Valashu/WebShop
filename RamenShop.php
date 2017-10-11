bc2f9a2a6801f1776c9a7a6dba8ef57069f1ad2e
Sebastian Andreas Werthemann <werts1@bfh.ch>
2017-10-11 12:53:14 +0200

    ramenupdate

--- RamenShop.php
+++ RamenShop.php
@@ -21,10 +21,8 @@
       <h1>Sidem3</h1>
   </div>
 
-  <div class=col-2>
+  <div class=col-6>
     <img src="images/ramen.png" alt="ramen"/>
-  </div>
-  <div class=col-5>
     <h1>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
