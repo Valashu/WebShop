47ce21331cac4302c3d925581d1a454c3a2bf327
Sebastian Andreas Werthemann <werts1@bfh.ch>
2017-10-11 14:08:58 +0200

    navedit

--- nav/nav.php
+++ nav/nav.php
@@ -1,4 +1,5 @@
 <ul class="topnav">
+  <li><a href="http://localhost/WebPrograms/WebShop/RamenShop.php" alt=home><div><div style="backgrounds-image: url(images/hs.jpg)"></div></div></a></li>
   <li><a href="http://localhost/WebPrograms/WebShop/RamenShop.php">Home</a></li>
   <li><a href="http://localhost/WebPrograms/WebShop/pages/ramen.php">Ramen</a></li>
   <li><a href="http://localhost/WebPrograms/WebShop/pages/about.php">About</a></li>
@@ -6,7 +7,7 @@
     <div class="dropcontent">
       <a href="#drop 1">Drop 1</a>
       <a href="#drop 2">Drop 2</a>
-      <a href="#drop 7.9">Drop 3</a>
+      <a href="#drop 3">Drop 3</a>
     </div>
   </li>
 </ul>
