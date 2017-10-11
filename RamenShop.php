6c3725038b03d35b8572ae3f9c791c65d62bba93
Shadowlink <Sordstriker_@hotmail.com>
2017-10-11 17:17:42 +0200

    responsive*

--- RamenShop.php
+++ RamenShop.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8"/>
 <link rel="stylesheet" media="all" type="text/css" href="cssreset.css"/>
-<link rel="stylesheet" media="all" type="text/css" href="responsible.css"/>
+<link rel="stylesheet" media="all" type="text/css" href="responsive.css"/>
 <link rel="stylesheet" media="all" type="text/css" href="nav/nav.css"/>
 <title>&#10643;Webshop&rpargt;</title>
 </head>
